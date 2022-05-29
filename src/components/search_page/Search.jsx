9a70a2fb31a28a7a738daf7503622cfda5f83024
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-05-29 13:55:12 +0530

    initial commit

---
+++ src/components/search_page/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search.scss';
+
+function Search() {
+  return (
+    <div className="Search" id="Search">
+      <div className="search_bar">
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
