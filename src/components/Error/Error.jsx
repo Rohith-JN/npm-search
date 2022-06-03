9b3da2131a268d1eb0b4012afa894d2a574c30d3
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-06-03 14:28:16 +0530

    Added error page

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './error.scss';
+
+function Error() {
+  return (
+    <main class="container">
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">4</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <span class="particle">0</span>
+      <article class="content">
+        <p><strong>Error (404)</strong></p>
+        <p>
+          Please enter a valid package name.
+        </p>
+      </article>
+    </main>
+  );
+}
+
+export default Error;
