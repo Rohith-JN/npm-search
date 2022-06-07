81cd2576169fdc4d78f6f296054fd2df1bcc99d9
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-06-07 10:50:20 +0530

    Finished readme component

--- src/components/Readme/Readme.tsx
+++ src/components/Readme/Readme.tsx
@@ -3,6 +3,12 @@ import './readme.scss';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import remarkMdx from 'remark-mdx';
+import remarkToc from 'remark-toc';
+import rehypeHighlight from 'rehype-highlight';
+import remarkSlug from 'remark-slug';
+import remarkEmoji from 'remark-emoji';
+import remarkMath from 'remark-math';
 
 interface ReadmeProps {
     input: string;
@@ -10,39 +16,49 @@ interface ReadmeProps {
 }
 
 const Readme: FC<ReadmeProps> = ({ owner, input }) => {
-    const [branch, setBranch] = useState({
-        branch: ''
-    });
+    const [url, seturl] = useState('');
     const [readme, setReadme] = useState('');
 
-    const fetchReadme = async (owner: string, input: string, branch: string) => {
-        const response = await fetch(`https://raw.githubusercontent.com/${owner}/${input}/${branch}/README.md`)
+    const fetchReadme = async (url: string) => {
+        const response = await fetch(url)
         const data = await response.text();
         if (response.status === 200) {
             setReadme(data);
         }
     }
 
-    const fetchBranch = async (owner: string, input: string) => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const fetchURL = async (owner: string, input: string) => {
         const response = await fetch(
-            `https://api.github.com/repos/${owner}/${input}`
+            `https://api.github.com/repos/${owner}/${input}/readme`
         );
         const data = await response.json();
-        setBranch({
-            branch: data.default_branch
-        });
+        seturl(data.download_url);
         if (response.status === 200) {
-            fetchReadme(owner, input, branch.branch);
+            fetchReadme(url);
+        }
+        else {
+            setReadme('No README.md found');
         }
     };
 
     useEffect(() => {
-        fetchBranch(owner, input);
-    }, [owner, input]);
+        fetchURL(owner, input);
+    }, [owner, input, fetchURL]);
 
     return (
         <div className='Readme' id='Readme'>
-            <ReactMarkdown children={readme} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
+            <ReactMarkdown children={readme} remarkPlugins={[
+                remarkGfm,
+                remarkToc,
+                remarkMath,
+                remarkMdx,
+                remarkEmoji,
+                remarkSlug
+            ]} rehypePlugins={[
+                rehypeRaw,
+                rehypeHighlight
+            ]} />
         </div>
     )
 }
--- src/components/main_page/Main.tsx
+++ src/components/main_page/Main.tsx
@@ -12,7 +12,7 @@ interface MainProps {
   input: string;
 }
 
-const Main:FC<MainProps> = ({ input }) => {
+const Main: FC<MainProps> = ({ input }) => {
   const [packageInfo, setPackageInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState({
@@ -26,7 +26,7 @@ const Main:FC<MainProps> = ({ input }) => {
     const response = await fetch(`https://api.npms.io/v2/package/${input}`);
     const data = await response.json();
     if (response.status === 200) {
-      setError({ error: false, errorMessage: '', errorCode: 200});
+      setError({ error: false, errorMessage: '', errorCode: 200 });
     } else if (response.status !== 200) {
       setError({
         error: true,
@@ -34,9 +34,11 @@ const Main:FC<MainProps> = ({ input }) => {
         errorCode: response.status,
       });
     } else {
-      setError({ error: true,
+      setError({
+        error: true,
         errorMessage: data.message,
-        errorCode: response.status, });
+        errorCode: response.status,
+      });
     }
 
     setPackageInfo(data);
@@ -60,38 +62,38 @@ const Main:FC<MainProps> = ({ input }) => {
             />
           ) : (
             <div className='main-container'>
-            <div className="row">
-              <Summary
-                heading={packageInfo.collected.metadata.name}
-                version={packageInfo.collected.metadata.version}
-                description={packageInfo.collected.metadata.description}
-                license={
-                  packageInfo.collected.metadata.license
-                    ? packageInfo.collected.metadata.license
-                    : 'No license'
-                }
-                npm={packageInfo.collected.metadata.links.npm}
-                github={packageInfo.collected.metadata.links.repository}
-                downloads={Math.trunc(
-                  packageInfo.evaluation.popularity.downloadsCount
-                ).toLocaleString()}
-                keywords={packageInfo.collected.metadata.keywords.join(', ')}
-              />
-              <LineChart input={input} />
-            </div>
-            <div className="row1">
-              <LineChart2 input={input} />
-              <PackageInfo 
-                stars={packageInfo.collected.github.starsCount}
-                forks={packageInfo.collected.github.forksCount} 
-                issues={packageInfo.collected.github.issues.openCount} 
-                contributors={packageInfo.collected.github.contributors.length} 
-                maintainers={packageInfo.collected.metadata.maintainers.length} 
-              />
-            </div>
-            <div className="row2">
-              <Readme input={input} owner={'facebook'} />
-            </div>
+              <div className="row">
+                <Summary
+                  heading={packageInfo.collected.metadata.name}
+                  version={packageInfo.collected.metadata.version}
+                  description={packageInfo.collected.metadata.description}
+                  license={
+                    packageInfo.collected.metadata.license
+                      ? packageInfo.collected.metadata.license
+                      : 'No license'
+                  }
+                  npm={packageInfo.collected.metadata.links.npm}
+                  github={packageInfo.collected.metadata.links.repository}
+                  downloads={Math.trunc(
+                    packageInfo.evaluation.popularity.downloadsCount
+                  ).toLocaleString()}
+                  keywords={packageInfo.collected.metadata.keywords.join(', ')}
+                />
+                <LineChart input={input} />
+              </div>
+              <div className="row1">
+                <LineChart2 input={input} />
+                <PackageInfo
+                  stars={packageInfo.collected.github.starsCount}
+                  forks={packageInfo.collected.github.forksCount}
+                  issues={packageInfo.collected.github.issues.openCount}
+                  contributors={packageInfo.collected.github.contributors.length}
+                  maintainers={packageInfo.collected.metadata.maintainers.length}
+                />
+              </div>
+              <div className="row2">
+                <Readme input={input} owner={packageInfo.collected.metadata.links.repository.split('/')[3]} />
+              </div>
             </div>
           )}
         </div>
