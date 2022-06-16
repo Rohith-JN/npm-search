9ab939bfe7d1fe37235d78bc8e7e55886c17ac69
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-06-16 19:43:29 +0530

    Fix Tailwind bug

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: [
-    "./src/**/*.{js,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
