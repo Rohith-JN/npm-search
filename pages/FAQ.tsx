32bf40e43a64861aa649b84b29d7ef36921bad26
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-07-03 11:39:30 +0530

    Made FAQ page responsive

--- pages/FAQ.tsx
+++ pages/FAQ.tsx
@@ -3,15 +3,15 @@ import { FC } from 'react';
 
 const FAQ: FC = () => {
     return (
-        <div className="container pl-20 py-24 mx-auto w-full h-screen bg-white dark:bg-gray-900">
+        <div className="pl-16 py-24 mx-auto w-full h-screen bg-white dark:bg-gray-900 sm:py-16">
             <Head>
                 <title>npm search | FAQ</title>
             </Head>
             <div className="flex flex-col items-center justify-center">
-                <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 dark:text-white mb-4">
+                <h1 className="text-2xl font-medium text-center title-font text-gray-900 dark:text-white mb-4">
                     Frequently Asked Questions
                 </h1>
-                <details className="mb-4 w-2/5 mt-6">
+                <details className="mb-4 w-2/5 mt-6 lg:w-4/5">
                     <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4 dark:text-black">
                         Where the downloads come from?
                     </summary>
@@ -19,7 +19,7 @@ const FAQ: FC = () => {
                         The npm package download data comes from npm&#39;s <a href="https://github.com/npm/download-counts">download counts</a> api and package details come from <a href="https://api-docs.npms.io/">npms.io</a>
                     </span>
                 </details>
-                <details className="mb-4 w-2/5">
+                <details className="mb-4 w-2/5 lg:w-4/5">
                     <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4 dark:text-black">
                         When the data is updated?
                     </summary>
@@ -28,7 +28,7 @@ const FAQ: FC = () => {
                         These statistics are not provided in real-time. All numbers will change at most once per day.
                     </span>
                 </details>
-                <details className="mb-4 w-2/5">
+                <details className="mb-4 w-2/5 lg:w-4/5">
                     <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4 dark:text-black">
                         Who is the creator of npm-search.com?
                     </summary>
@@ -37,7 +37,7 @@ const FAQ: FC = () => {
                         <a href="https://github.com/Rohith-JN">Rohith JN</a> started the project
                     </span>
                 </details>
-                <details className="mb-4 w-2/5">
+                <details className="mb-4 w-2/5 lg:w-4/5">
                     <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4 dark:text-black">
                         Can I participate to improve the website?
                     </summary>
