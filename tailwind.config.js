cfb39e938717f31130c954945c9a60b680c04bfd
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-07-03 11:38:30 +0530

    Added custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", 
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: ['class'],
   theme: {
     typography: (theme) => ({}),
-    extend: {},
+    extend: {}, 
+    screens: {
+      'xsm': {'max': '300px'},
+      'sm': {'max': '350px'},
+      'xmd': {'max': '390px'},
+      'md': {'max': '600px'},
+      'slg': {'max': '700px'},
+      'lg': {'max': '960px'},
+      'xlg': {'max': '1199px'},
+      'xl': {'max': '1200px'},
+    }
   },
   variants: {},
   plugins: [require('@tailwindcss/typography')],
