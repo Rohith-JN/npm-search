be979217dca0f462379312948dac39402bc8660e
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-07-13 09:38:52 +0530

    Added meta-tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta name="description" content="A site to view and compare npm-package statistics" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
